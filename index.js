e1afbdbff54845842a864fb97a704d90ea171d57
Jae Huh <jaeeunhuh@gmail.com>
2017-04-30 13:41:59 +1200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const routes = require('./routes')
+
+const PORT = 3000
+const app = express()
+
+app.listen(PORT, function () {
+  console.log('The server is listening on port', PORT)
+})
+
+app.get('/', routes.hello)
---
+++ routes.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+function hello (req, res) {
+  res.send('HELLO!')
+}
+
+module.exports = {
+  hello
+}
