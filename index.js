437f066211c4f5c05ed2b59c86e364d49e229303
Jae Huh <jaeeunhuh@gmail.com>
2017-04-30 14:54:27 +1200

    Add jokes to an object and displays the object

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const routes = require('./routes')
+const app = express()
 
 const PORT = 3000
-const app = express()
 
 app.listen(PORT, function () {
   console.log('The server is listening on port', PORT)
 })
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', routes.addJokes)
+app.post('/jokes', routes.jokes)
--- routes.js
+++ routes.js
@@ -4,6 +4,16 @@ function addJokes (req, res) {
   res.sendFile(path.join(__dirname, 'public', 'add-jokes.html'))
 }
 
+function jokes (req, res) {
+  const collection = {}
+  const question = req.body.question
+  const answer = req.body.answer
+  collection.question = question
+  collection.answer = answer
+  res.send(collection)
+}
+
 module.exports = {
-  addJokes
+  addJokes,
+  jokes
 }
